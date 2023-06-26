28b866e978e638fadd262835bd60d3b4235cd28c
DDidas <101012907+DDidas@users.noreply.github.com>
2023-06-26 14:45:02 +0200

    Kommentare

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-var cardsFlipped = [];
-var matchesFound = 0;
-var numPairs = 4;
-var numCards;
-var board = document.getElementById('game-board');
-var imgIndices;
+var cardsFlipped = []; // Array zum Speichern der umgedrehten Karten
+var matchesFound = 0; // Anzahl der gefundenen Paare
+var numPairs = 4; // Anzahl der Paare (Standardwert: 4)
+var numCards; // Gesamtanzahl der Karten
+var board = document.getElementById('game-board'); // Spielbrett-Element
+var imgIndices; // Array zum Speichern der Indizes der Bilder
 var players = []; // Array für Spieler
 var currentPlayer = 0; // Aktueller Spieler
-var gameInfo = document.getElementById('game-info');
+var gameInfo = document.getElementById('game-info'); // Spielinfo-Element
 var isPairMatched = false; // Flag, um zu überprüfen, ob das aufgedeckte Paar übereinstimmt
 
-var configForm = document.getElementById('config-form');
-configForm.addEventListener('submit', startGame);
+var configForm = document.getElementById('config-form'); // Konfigurationsformular
+configForm.addEventListener('submit', startGame); // Eventlistener für das Formular
 
+// Funktion zum Starten des Spiels
 function startGame(event) {
-    document.getElementById('game-board').style.visibility = "visible";
-    event.preventDefault();
-    numPairs = parseInt(document.getElementById('pairs-input').value);
-    var numPlayers = parseInt(document.getElementById('players-input').value);
-    numCards = numPairs * 2;
-    resetGame();
-    var colors = ['red', 'blue', 'green', 'purple', 'orange', 'cyan', 'pink', 'yellow', 'brown', 'magenta', 'teal', 'olive', 'maroon', 'navy', 'aqua'];
-    createPlayers(numPlayers, colors);
-    createBoard();
-    displayGameInfo(colors);
+    document.getElementById('game-board').style.visibility = "visible"; // Sichtbarkeit des Spielbretts setzen
+    event.preventDefault(); // Verhindert das Standardverhalten des Formulars (Seitenneuladung)
+    numPairs = parseInt(document.getElementById('pairs-input').value); // Anzahl der Paare aus dem Formular lesen und parsen
+    var numPlayers = parseInt(document.getElementById('players-input').value); // Anzahl der Spieler aus dem Formular lesen und parsen
+    numCards = numPairs * 2; // Gesamtanzahl der Karten berechnen
+    resetGame(); // Spiel zurücksetzen
+    var colors = ['red', 'blue', 'green', 'purple', 'orange', 'cyan', 'pink', 'yellow', 'brown', 'magenta', 'teal', 'olive', 'maroon', 'navy', 'aqua']; // Farben für die Spieler
+    createPlayers(numPlayers, colors); // Spieler erstellen
+    createBoard(); // Spielbrett erstellen
+    displayGameInfo(colors); // Spielinfo anzeigen
 }
 
+// Funktion zum Zurücksetzen des Spiels
 function resetGame() {
-    cardsFlipped = [];
-    matchesFound = 0;
-    players = [];
-    currentPlayer = 0;
-    isPairMatched = false;
-    board.innerHTML = '';
-    gameInfo.innerHTML = '';
+    cardsFlipped = []; // Array für umgedrehte Karten leeren
+    matchesFound = 0; // Anzahl der gefundenen Paare zurücksetzen
+    players = []; // Spieler-Array leeren
+    currentPlayer = 0; // Aktuellen Spieler zurücksetzen
+    isPairMatched = false; // Flag für übereinstimmendes Paar zurücksetzen
+    board.innerHTML = ''; // Spielbrett leeren
+    gameInfo.innerHTML = ''; // Spielinfo leeren
 }
 
+// Funktion zum Erstellen der Spieler
 function createPlayers(numPlayers, colors) {
     for (var i = 0; i < numPlayers; i++) {
         players.push({
@@ -46,15 +49,17 @@ function createPlayers(numPlayers, colors) {
     }
 }
 
+// Funktion zum Erstellen des Spielbretts
 function createBoard() {
-    imgIndices = createShuffledIndices();
+    imgIndices = createShuffledIndices(); // Zufällige Indizes für die Bilder erstellen
     for (var i = 0; i < numCards; i++) {
-        var card = createCard(imgIndices[i]);
-        card.addEventListener('click', handleCardClick);
-        board.appendChild(card);
+        var card = createCard(imgIndices[i]); // Karte erstellen
+        card.addEventListener('click', handleCardClick); // Eventlistener für Klick auf die Karte hinzufügen
+        board.appendChild(card); // Karte dem Spielbrett hinzufügen
     }
 }
 
+// Funktion zum Anzeigen der Spielinformationen
 function displayGameInfo() {
     for (var i = 0; i < players.length; i++) {
         var playerInfo = document.createElement('div');
@@ -65,7 +70,7 @@ function displayGameInfo() {
     updateCurrentPlayer();
 }
 
-
+// Funktion zum Aktualisieren des aktuellen Spielers
 function updateCurrentPlayer() {
     var playerInfos = gameInfo.getElementsByClassName('player-info');
     for (var i = 0; i < playerInfos.length; i++) {
@@ -77,15 +82,16 @@ function updateCurrentPlayer() {
     }
 }
 
+// Funktion zum Behandeln des Klicks auf eine Karte
 function handleCardClick(e) {
     var currentCard = e.target;
     if (cardsFlipped.length < 2 && !currentCard.classList.contains('flipped')) {
         flipCard(currentCard);
         if (cardsFlipped.length === 2) {
-            players[currentPlayer].moves++; // Zähle den Zug für den aktuellen Spieler
+            players[currentPlayer].moves++; // Zug für aktuellen Spieler zählen
             updateMovesCounter();
             if (!isPairMatched) {
-                switchPlayer(); // Wechsel zum nächsten Spieler
+                switchPlayer(); // Zum nächsten Spieler wechseln
             }
             checkForMatch();
         }
@@ -93,11 +99,11 @@ function handleCardClick(e) {
     checkIfGameOver();
 }
 
-
+// Funktion zum Überprüfen, ob das Spiel beendet ist
 function checkIfGameOver() {
     if (matchesFound === numPairs) {
         setTimeout(function() {
-            var winner = getWinner();
+            var winner = getWinner(); // Gewinner ermitteln
             var message = '';
             if (winner === -1) {
                 message = 'Unentschieden! Kein Spieler hat gewonnen.';
@@ -109,11 +115,13 @@ function checkIfGameOver() {
     }
 }
 
+// Funktion zum Umdrehen einer Karte
 function flipCard(card) {
     card.classList.add('flipped');
     cardsFlipped.push(card);
 }
 
+// Funktion zum Überprüfen auf ein Paar
 function checkForMatch() {
     setTimeout(function() {
         var value1 = cardsFlipped[0].dataset.value;
@@ -122,25 +130,26 @@ function checkForMatch() {
             markAsMatched(cardsFlipped[0]);
             markAsMatched(cardsFlipped[1]);
             matchesFound++;
-            players[currentPlayer].pairsFound++; // Zähle das aufgedeckte Paar für den aktuellen Spieler
+            players[currentPlayer].pairsFound++; // Aufgedecktes Paar für aktuellen Spieler zählen
             updatePairsCounter();
-            isPairMatched = true; // Das aufgedeckte Paar stimmt überein
+            isPairMatched = true; // Aufgedecktes Paar stimmt überein
         } else {
             unflipCards(cardsFlipped[0], cardsFlipped[1]);
-            isPairMatched = false; // Das aufgedeckte Paar stimmt nicht überein
+            isPairMatched = false; // Aufgedecktes Paar stimmt nicht überein
         }
         cardsFlipped = [];
-        checkIfGameOver(); // Überprüfen Sie hier auf Spielende
+        checkIfGameOver(); // Überprüfen, ob das Spiel beendet ist
     }, 1000);
 }
 
-
+// Funktion zum Markieren einer Karte als gefunden
 function markAsMatched(card) {
     var currentPlayerColor = players[currentPlayer].color;
     card.style.border = '2px solid ' + currentPlayerColor;
     card.classList.add('matched');
 }
 
+// Funktion zum Zurückdrehen von Karten
 function unflipCards(card1, card2) {
     setTimeout(function() {
         card1.classList.remove('flipped');
@@ -148,6 +157,7 @@ function unflipCards(card1, card2) {
     }, 1000);
 }
 
+// Funktion zum Mischen eines Arrays
 function shuffleArray(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex > 0) {
@@ -160,6 +170,7 @@ function shuffleArray(array) {
     return array;
 }
 
+// Funktion zum Erstellen von zufälligen Indizes für die Bilder
 function createShuffledIndices() {
     var indices = [];
     for (var i = 0; i < numPairs; i++) {
@@ -168,6 +179,7 @@ function createShuffledIndices() {
     return shuffleArray(indices);
 }
 
+// Funktion zum Erstellen einer Karte
 function createCard(value) {
     var card = document.createElement('div');
     card.classList.add('card');
@@ -176,7 +188,7 @@ function createCard(value) {
     return card;
 }
 
-
+// Funktion zum Ermitteln des Gewinners
 function getWinner() {
     var maxPairsFound = Math.max(...players.map(player => player.pairsFound));
     var playersWithMaxPairs = players.filter(player => player.pairsFound === maxPairsFound);
@@ -190,7 +202,7 @@ function getWinner() {
     }
 }
 
-
+// Funktion zum Aktualisieren des Zug-Zählers
 function updateMovesCounter() {
     var movesCounters = gameInfo.getElementsByClassName('moves-counter');
     for (var i = 0; i < movesCounters.length; i++) {
@@ -198,6 +210,7 @@ function updateMovesCounter() {
     }
 }
 
+// Funktion zum Aktualisieren des Paar-Zählers
 function updatePairsCounter() {
     var pairsCounters = gameInfo.getElementsByClassName('pairs-counter');
     for (var i = 0; i < pairsCounters.length; i++) {
@@ -205,6 +218,7 @@ function updatePairsCounter() {
     }
 }
 
+// Funktion zum Wechseln des aktuellen Spielers
 function switchPlayer() {
     currentPlayer++;
     if (currentPlayer === players.length) {
